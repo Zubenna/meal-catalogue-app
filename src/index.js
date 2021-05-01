9399e1cec281f127732e606d33b8187c4ff0ca7b
Zubenna <emelunna@gmail.com>
2021-05-01 15:01:25 +0100

    Installed Bootstrap

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 // import './styles/index.css';
 // import App from './components/App';
 import rootReducer from './reducers';
