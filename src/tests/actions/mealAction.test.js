c8f246c5e1131ffa113596730f57b107b37aff92
Zubenna <emelunna@gmail.com>
2021-05-06 09:09:53 +0100

    Completed Test for redux actions

--- src/tests/actions/mealAction.test.js
+++ src/tests/actions/mealAction.test.js
@@ -11,3 +11,43 @@ describe('actions/mealAction', () => {
       expect(result.type).toBe(ActionTypes.DISPLAY_MEALS);
     });
 });
+
+describe('actions/selectedMeal', () => {
+  let result;
+
+  beforeEach(() => {
+    result = selectedMeal('meal');
+  });
+
+  test('contains SELECTED_MEAL variable', () => {
+    expect(ActionTypes.SELECTED_MEAL).toBe('SELECTED_MEAL');
+  });
+
+  test('selectedMeal returns an object with payload', () => {
+    expect(result.payload).toBe('meal');
+  });
+
+  test('selectedMeal returns an object with type', () => {
+    expect(result.type).toBe(ActionTypes.SELECTED_MEAL);
+  });
+});
+
+describe('actions/filterChange', () => {
+  let result;
+
+  beforeEach(() => {
+    result = filterChange('area');
+  });
+
+  test('contains FILTER_CHANGE variable', () => {
+    expect(ActionTypes.FILTER_CHANGE).toBe('FILTER_CHANGE');
+  });
+
+  test('filterChange returns an object with payload', () => {
+    expect(result.filter).toBe('area');
+  });
+
+  test('filterChange returns an object with type', () => {
+    expect(result.type).toBe(ActionTypes.FILTER_CHANGE);
+  });
+});
